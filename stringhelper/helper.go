c4dcd7efacca32d55b5fe053dfe7c5e6b91131d6
Zeromaru <ryuookami.zeromaru@gmail.com>
2018-07-21 21:44:31 +0700

    second commit

--- stringhelper/helper.go
+++ stringhelper/helper.go
@@ -1,6 +1,17 @@
 package stringhelper
 
+import "fmt"
+
 // Upper function
 func Upper(text string) string {
 	return "CAT"
+}
+
+// Concat function
+func Concat(inputs ...string) string {
+	var result string
+	for _, input := range inputs {
+		result = fmt.Sprintf("%s %s", result, input)
+	}
+	return result
 }
